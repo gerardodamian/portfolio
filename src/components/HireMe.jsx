da2e352b523d370faa309538206d28332e10517f
Gerardo Montini <germontini@hotmail.com>
2024-02-21 19:41:39 -0300

    correo

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -63,7 +63,7 @@ const HireMe = () => {
                     <div className="mt-32 flex items-center space-x-6">
                         {/* <!-- Mail --> */}
                         <a
-                            href="gerardomontini@gmail.com"
+                            href="mailto:gerardomontini@gmail.com"
                             className="text-secondary underline mt-3 block"
                         >
                             <img
